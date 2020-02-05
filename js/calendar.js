f10ba576083a63d4ab40fe5788d20872c3c353cc
hector <hectorsandovalcordon@gmail.com>
2020-02-05 13:33:10 +0100

    update

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,16 @@
     $(document).ready(function () {
         $("#my-calendar").zabuto_calendar({
             language: "es",
+            action: function () {
+                return myDateFunction(this.id, false);
+            },
+            action_nav: function () {
+                return myNavFunction(this.id);
+            },
             legend: [
-                {type: "text", label: "Special event", badge: "00"},
-                {type: "block", label: "Regular event", classname: "purple"},
-                {type: "spacer"},
-                {type: "text", label: "Bad"},
-                {type: "list", list: ["grade-1", "grade-2", "grade-3", "grade-4"]},
-                {type: "text", label: "Good"}
+                {type: "text", label: "Competición Jaca", badge: "26 de Enero"},
+                {type: "text", label: "Competición Panel", badge: "Marzo 2020"},
+                {type: "text", label: "Quedada Albarracín", badge: "Diciembre 2020"},
               ],
             data: [
                 {
@@ -29,3 +32,26 @@
             }
                  })
     });
+
+    function myDateFunction(id, fromModal) {
+        $("#date-popover").hide();
+        if (fromModal) {
+            $("#" + id + "_modal").modal("hide");
+        }
+        var date = $("#" + id).data("date");
+        var hasEvent = $("#" + id).data("hasEvent");
+        if (hasEvent && !fromModal) {
+            return false;
+        }
+        $("#date-popover-content").html('You clicked on date ' + date);
+        $("#date-popover").show();
+        return true;
+    }
+
+    function myNavFunction(id) {
+        $("#date-popover").hide();
+        var nav = $("#" + id).data("navigation");
+        var to = $("#" + id).data("to");
+        console.log('nav ' + nav + ' to: \ + to.month + '/' + to.year')
+}
+
